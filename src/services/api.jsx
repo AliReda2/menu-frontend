f62d1c3dee139034d337eb487c655de85dce2d5f
AliReda2 <alireda2572003@gmail.com>
2025-02-16 13:18:22 +0200

    .

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -6,20 +6,4 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// Add a request interceptor to attach shop_id dynamically in the URL path
-api.interceptors.request.use(
-  (config) => {
-    // Split the pathname, filter empty segments, and take the last one
-    const segments = window.location.pathname.split('/').filter(Boolean);
-    const shopId = segments[segments.length - 1]; // Get the last section as shopId
-    if (shopId) {
-      config.url = `${shopId}`;
-    }
-    console.log("Extracted shopId:", shopId);
-    // console.log("Request config:", config);
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
 export default api;
