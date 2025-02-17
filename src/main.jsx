73f2a83562cd82fc31a2b2fe2d3eb7d9134488c9
AliReda2 <alireda2572003@gmail.com>
2025-02-17 10:33:40 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GlobalProvider } from "./context/GlobalState"; // Import GlobalProvider
+import { GlobalStateProvider } from "./context/GlobalState"; // Import GlobalStateProvider
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalProvider>
+    <GlobalStateProvider>
       <App />
-    </GlobalProvider>
+    </GlobalStateProvider>
   </StrictMode>
 );
