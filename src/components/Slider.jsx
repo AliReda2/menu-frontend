7c649bb8a342f27f8f0e7e9ca4908f5c8b939c8c
AliReda2 <alireda2572003@gmail.com>
2025-02-18 19:00:35 +0200

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react";
 import "../style/Slider.css";
 
 const Slider = ({ shop }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = `https://menu-backend-rnpu.onrender.com/${shop.image}`;
+    img.onload = () => {
+      setImageSrc(img.src);
+      setImageLoaded(true);
+    };
+  }, [shop.image]);
+
   return (
     <div
       className="slider"
-      style={{
-        "--background-image": `url(https://menu-backend-rnpu.onrender.com/${shop.image})`,
-      }}
+      style={{ backgroundImage: imageLoaded ? `url(${imageSrc})` : "none" }}
     >
+      {!imageLoaded && <div className="placeholder">Loading...</div>}
       <div className="slide">
         <h1>Welcome to {shop.name}</h1>
         <p>{shop.description}</p>
