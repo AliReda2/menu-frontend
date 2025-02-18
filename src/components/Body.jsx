b3051d125e1c141917bc861f550e872c56dc4a55
AliReda2 <alireda2572003@gmail.com>
2025-02-18 20:04:25 +0200

    .

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import api from "../services/api";
 import "../style/Body.css";
 import Categories from "./Categories";
