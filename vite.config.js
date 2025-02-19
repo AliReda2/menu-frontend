b4f6f90f97bbf97899e7113b2bcaa6c72f9e0353
AliReda2 <alireda2572003@gmail.com>
2025-02-19 11:42:44 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Inspect from 'vite-plugin-inspect';
-
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react(),Inspect()], // Removed Tailwind CSS plugin
+  plugins: [react(), viteCompression()], // Removed Tailwind CSS plugin
   server: {
-    port: 3000,
     proxy: {
       "/api": {
         target: "https://menu-backend-rnpu.onrender.com",
         changeOrigin: true,
+        secure: true, // Ensures HTTPS is properly handled
         rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true, // Removes console logs
+        drop_debugger: true, // Removes debugger statements
       },
     },
   },
