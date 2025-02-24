4ff04e0b7235d76009d632e76a5e00addd21a016
AliReda2 <alireda2572003@gmail.com>
2025-02-24 16:57:45 +0200

    .

--- src/components/admin/ShopUpdate.jsx
+++ src/components/admin/ShopUpdate.jsx
@@ -73,7 +73,17 @@ const ShopUpdate = ({ shopId, shop }) => {
   };
 
   return (
-    <Container sx={{ p: 0 }}>
+    <Container
+      sx={{
+        paddingLeft: 0,
+        paddingRight: 0,
+        "& .MuiContainer-root": {
+          paddingLeft: "0 !important",
+          paddingRight: "0 !important",
+        },
+      }}
+    >
+      {" "}
       <Card>
         <CardContent>
           <Typography variant="h5" gutterBottom>
