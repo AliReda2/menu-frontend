0745d4ba02c7a8b9dfcd08e3c54384fea2dfff51
AliReda2 <alireda2572003@gmail.com>
2025-02-24 15:39:02 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { GlobalStateProvider } from "./context/GlobalState";
+import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
 import App from "./App";
-import { GlobalStateProvider } from "./context/GlobalState"; // Import GlobalStateProvider
+import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStateProvider>
-      <App />
-    </GlobalStateProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStateProvider>
+        <App />
+      </GlobalStateProvider>
+    </ThemeProvider>
   </StrictMode>
 );
